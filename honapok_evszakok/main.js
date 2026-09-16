69b3eeda88bace79a44f3703c236dde24345d4f7
melcherlevente <melcherlevente@turr.hu>
2026-09-16 11:07:41 +0200

    1. honapok tomb

---
+++ honapok_evszakok/main.js
@@ -0,0 +1,15 @@
+let monthInput = document.getElementById("monthInput").value;
+let months = [
+    {month: "Január", season: "Tél", days: 31},
+    {month: "Február", season: "Tél", days: 28},
+    {month: "Március", season: "Tavasz", days: 31},
+    {month: "Április", season: "Tavasz", days: 30},
+    {month: "Május", season: "Tavasz", days: 31},
+    {month: "Június", season: "Nyár", days: 30},
+    {month: "Július", season: "Nyár", days: 31},
+    {month: "Augusztus", season: "Nyár", days: 31},
+    {month: "Szeptember", season: "Ősz", days: 30},
+    {month: "Október", season: "Ősz", days: 31},
+    {month: "November", season: "Ősz", days: 30},
+    {month: "December", season: "Tél", days: 31}
+];
