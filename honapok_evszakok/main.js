1f03bca91a12069fe6e7bdeb250aec7b9863e901
melcherlevente <melcherlevente@turr.hu>
2026-09-21 14:03:39 +0200

    modositas oldal, unnep valtoztatas

--- honapok_evszakok/main.js
+++ honapok_evszakok/main.js
@@ -29,7 +29,6 @@ function start(){
             throw new Error("Nem számot adtál meg!")
         }
         else if(month < 1 || month > 12){
-            
             throw new Error("A hónapszámnak 1 és 12 közé kell esnie!");
         }
         
@@ -41,4 +40,49 @@ function start(){
         output.textContent = "Hiba: " + error.message;
     }
     document.getElementById("monthInput").value = "";
+}
+
+function modifyHoliday(){
+    document.getElementById("outputMonth").textContent = ""
+    document.getElementById("outputSeason").textContent = ""
+    document.getElementById("outputDays").textContent = ""
+    document.getElementById("outputHoliday").textContent = ""
+    try{
+        let month = Number(document.getElementById("monthInput").value);
+
+        if(isNaN(month)){
+            throw new Error("Nem számot adtál meg!")
+        }
+        else if(month < 1 || month > 12){
+            
+            throw new Error("A hónapszámnak 1 és 12 közé kell esnie!");
+        }
+        
+        let monthID = getMonthInfo(month);
+        document.getElementById("outputMonth").textContent = `Hónap: ${monthID.month}`
+        document.getElementById("outputSeason").textContent = `Évszak: ${monthID.season}`
+        document.getElementById("outputDays").textContent = `Napok száma: ${monthID.days}`
+
+        let holidayModify = document.createElement("input")
+        holidayModify.value = months[month-1].holiday
+        document.getElementById("outputHoliday").textContent = `Ünnep:`
+        document.getElementById("outputHoliday").appendChild(holidayModify)
+
+        let save = document.createElement("button")
+        save.textContent = "Mentés"
+        save.onclick = function mentes(){
+            months[month-1].holiday = holidayModify.value
+            let newHoliday = getMonthInfo(month)
+            document.getElementById("outputNewHoliday").textContent = `Új ünnep: ${newHoliday.holiday}`
+        }
+        document.getElementById("outputHoliday").appendChild(save)
+        document.getElementById("outputNewHoliday").textContent = ""
+        
+    }
+    catch(error){
+        outputMonth.textContent = "Hiba: " + error.message;
+    }
+    document.getElementById("monthInput").value = "";
+    
+
 }
